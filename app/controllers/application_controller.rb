8b8aa4aaf3f328a13ce38f0d7535c191db319505
jacksharplessE <jackson.sharpless2@gmail.com>
2019-07-11 20:29:20 -0400

    changed rendered text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "goodbye, world!"
+    render html: "I wonder if something new will show up"
   end
 end
