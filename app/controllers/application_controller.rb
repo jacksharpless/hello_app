e9621328bc1c74d82002fa1ffa0bef8da70709a7
jacksharplessE <jackson.sharpless2@gmail.com>
2019-07-11 20:41:35 -0400

    new change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "I wonder if something new will show up"
+    render html: "This website is by Jackson Sharpless, its the first thing he has put on the internet. Its very plain, but its live!"
   end
 end
